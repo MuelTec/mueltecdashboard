6946f0dc86460b04a510068ac29e1b80e6696bff
Samuel Paulo <112398583+MuelTec@users.noreply.github.com>
2023-06-22 16:33:40 -0300

    loja

---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+
+let pecas = [
+    {
+      "id": 1,
+      "nome": "GTX 1050",
+      "fabricante": "Nvidia",
+      "quantidade": 10,
+      "preco": 1000,
+      "categoria": "processador",
+      "imagem":"https://www.nvidia.com/content/dam/en-zz/Solutions/about-nvidia/logo-and-brand/02-nvidia-logo-color-grn-500x200-4c25-p@2x.png"
+      
+    },
+    {
+      "id": 2,
+      "nome": "Ryzen 5 3600",
+      "fabricante": "AMD",
+      "quantidade": 2,
+      "preco": 600,
+      "categoria": "processador",
+      "imagem":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/AMD_Logo.svg/800px-AMD_Logo.svg.png"
+    },
+    {
+        "id": 1,
+        "nome": "GTX 1050",
+        "fabricante": "Nvidia",
+        "quantidade": 10,
+        "preco": 1000,
+        "categoria": "processador",
+        "imagem":"https://www.nvidia.com/content/dam/en-zz/Solutions/about-nvidia/logo-and-brand/02-nvidia-logo-color-grn-500x200-4c25-p@2x.png"
+        
+      },
+      {
+        "id": 2,
+        "nome": "Ryzen 5 3600",
+        "fabricante": "AMD",
+        "quantidade": 2,
+        "preco": 600,
+        "categoria": "processador",
+        "imagem":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/AMD_Logo.svg/800px-AMD_Logo.svg.png"
+      },
+      {
+        "id": 1,
+        "nome": "GTX 1050",
+        "fabricante": "Nvidia",
+        "quantidade": 10,
+        "preco": 1000,
+        "categoria": "processador",
+        "imagem":"https://www.nvidia.com/content/dam/en-zz/Solutions/about-nvidia/logo-and-brand/02-nvidia-logo-color-grn-500x200-4c25-p@2x.png"
+        
+      },
+      {
+        "id": 2,
+        "nome": "Ryzen 5 3600",
+        "fabricante": "AMD",
+        "quantidade": 2,
+        "preco": 600,
+        "categoria": "processador",
+        "imagem":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/AMD_Logo.svg/800px-AMD_Logo.svg.png"
+      },
+      {
+        "id": 1,
+        "nome": "GTX 1050",
+        "fabricante": "Nvidia",
+        "quantidade": 10,
+        "preco": 1000,
+        "categoria": "processador",
+        "imagem":"https://www.nvidia.com/content/dam/en-zz/Solutions/about-nvidia/logo-and-brand/02-nvidia-logo-color-grn-500x200-4c25-p@2x.png"
+        
+      },
+      {
+        "id": 2,
+        "nome": "Ryzen 5 3600",
+        "fabricante": "AMD",
+        "quantidade": 2,
+        "preco": 600,
+        "categoria": "processador",
+        "imagem":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/AMD_Logo.svg/800px-AMD_Logo.svg.png"
+      }
+      
+      
+      
+    
+  ]
+buscarPecas()
+
+async function buscarPecas() {
+    exibirPecasNaTela(pecas)
+}
+
+
+
